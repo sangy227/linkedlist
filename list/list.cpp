9f537d9f3c1ffaf25006e080a60dc5c68cfafcfb
sangy227 <sangy227@gmail.com>
2024-08-27 22:21:49 +0900

    Initial commit

---
+++ list/list.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+
+
+namespace sy {
+    template <typename T>
+    class list
+    {
+    public:
+        struct Node
+        {
+            Node* Prev;
+            T Data;
+            Node* Next;
+        };
+        list()
+        {
+           
+        }
+        ~list()
+        {
+            mHead = nullptr;
+            mTail = nullptr;
+        }
+
+        void pushback(T _data)
+        {
+            if (mHead == nullptr) //초기값
+            {
+                mHead = new Node();
+                mHead->Data = _data;
+                mHead->Next = nullptr;
+                mHead->Prev = nullptr;
+
+                mTail = mHead;
+            }
+            else
+            {
+                mTail->Next = new Node();
+                mTail->Next->Data = _data;
+
+                mTail->Next->Next = nullptr;
+                mTail->Next->Prev = this->mTail;
+
+                mTail = mTail->Next;
+            }
+        }
+
+        void erase()
+        {
+            if (mTail->Prev == nullptr) // 노드가 첫 노드부분일때?
+                return;
+            else
+            {
+                Node* mDel = mTail;
+                mTail = mTail->Prev;
+                mTail->Next = nullptr;
+
+                delete mDel;
+                mDel = nullptr;
+            }
+        }
+
+    private:
+        Node* mHead;
+        Node* mTail;
+    };
+}
+int main()
+{
+    sy::list<int> mList;
+    mList.pushback(1);
+    mList.pushback(2);
+    mList.pushback(3);
+    int a = 0;
+
+    mList.erase();
+    mList.erase();
+    mList.erase();
+    
+  
+}
+
